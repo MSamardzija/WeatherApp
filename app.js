76ee7c3d78c75d88b587f39e507ea61c45b3cf0a
MSamardzija <marko1716@its.edu.rs>
2021-12-18 14:09:45 +0100

    Update time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const timeEl = document.getElementById("time");
 const dateEl = document.getElementById("date");
 const currentWeatherItemsEl = document.getElementById("current-weather-items");
 const timezone = document.getElementById("time-zone");
-const countryEl = document.getElementById("time-zone");
+const countryEl = document.getElementById("country");
 const weatherForecastEl = document.getElementById("weather-forecast");
 const currentTempEl = document.getElementById("current-temp");
 
@@ -17,11 +17,12 @@ setInterval(() => {
   const date = time.getDate();
   const day = time.getDay();
   const hour = time.getHours();
-  const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour;
+  const hoursIn12HrFormat = hour >= 13 ? hour %12: hour;
   const minutes = time.getMinutes();
   const ampm = hour >= 12 ? "PM" : "AM";
   // Time
-  timeEl.innerHTML = hoursIn12HrFormat + ":" + minutes + " " + `<span id="am-pm">${ampm}</span>`;
+  timeEl.innerHTML = (hoursIn12HrFormat < 10? '0' + hoursIn12HrFormat : hoursIn12HrFormat)
+   + ":" + (minutes < 10? '0' +minutes: minutes) + " " + `<span id="am-pm">${ampm}</span>`;
   // Date
   dateEl.innerHTML = days[day] + ", " + date + " " + months[month];
 }, 1000);
@@ -75,7 +76,7 @@ function showWeatherData(data){
             <div class="other">
                 <div class="day">${window.moment(day.dt * 1000).format("ddd")}</div>
                 <div class="temp">Night - ${day.temp.night}&#176; C</div>
-                <div class="temp">Day - 3${day.temp.day}#176; C</div>
+                <div class="temp">Day - ${day.temp.day}&#176; C</div>
             </div>
             `
         }else{
